c6e381199e8e69a5625cef61f64dada21fba5503
Aizon <killian.bonneau@iia-formation.fr>
2024-10-08 11:39:51 +0200

    Add tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import './styles/main.css'
+
 function App() {
   return (
     <>
-      <h1>henlo</h1>
+        <form action="">
+            <label htmlFor="name" className='text-9xl'>Nom de la tâche</label>
+            <input type="text" name="name" id="name" />
+            <label htmlFor="dueDate">Date d'échéance</label>
+            <input type='date' name='dueDate' id='dueDate' />
+            <button type="submit">Créer une tâche</button>
+        </form>
     </>
   );
 }
