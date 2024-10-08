1c3dad593b82fa1b0b410c0e2814f746de309abf
Aizon <killian.bonneau@iia-formation.fr>
2024-10-08 12:12:18 +0200

    Update styling to be ✨ purple ✨

--- src/App.tsx
+++ src/App.tsx
@@ -37,14 +37,15 @@ function App() {
 
     return (
         <>
+            <h1 className='flex justify-center text-4xl text-violet-500 underline decoration-dotted font-bold font-sans mt-12'>Cahier de texte</h1>
             <form action="" className='flex flex-col justify-center space-y-2 mt-12 p-4 max-w-80 gap-4 border shadow-lg mx-auto rounded-lg' onSubmit={handleSubmit}>
                 <label htmlFor="name">Nom de la tâche</label>
                 <input type="text" name="name" id="name" placeholder='Nom de la tâche' className='border p-2' value={title} onChange={e => setTitle(e.target.value)} />
                 <label htmlFor="dueDate">Date d'échéance</label>
                 <input type='datetime-local' name='dueDate' id='dueDate' placeholder='Date d&quot;échéance' className='border p-2' value={dueDate} onChange={e => setDueDate(e.target.value)} />
-                <button type="submit" className='justify-self-center grow p-2 m-4 bg-slate-100 rounded-lg'>Créer une tâche</button>
+                <button type="submit" className='justify-self-center grow bg-violet-300 p-2 m-4 rounded-lg'>Créer une tâche</button>
             </form>
-            {message !== '' && <p className='text-center mt-4'>{message}</p>}
+            {message !== '' && <p className='text-center text-violet-800 mt-4'>{message}</p>}
         </>
     );
 }
